f55f061187cf1977b4b635d7038676167bfd4487
alanwikid <alanwik@gmail.com>
2014-04-29 17:03:35 -0300

    Model for list Items and associated migration

---
+++ lists/migrations/0001_initial.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
